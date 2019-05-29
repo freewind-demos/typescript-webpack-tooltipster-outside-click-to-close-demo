aa28bc63ed23ad78a4cb1865d2e9d9c3a78fbe8e
freewind <nowindlee@gmail.com>
2019-05-29 22:19:13 +0800

    fix

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ TypeScript Webpack Tooltipster Outside Click to Close Demo
 
 在tooltip外click来关闭tooltipster
 
+需要注意：
+
+1. `body`的height一定要设置大一点，比如`100%`，否则在tooltip外`click`，实际上不生效。这个问题卡了我几个小时。
+2. tooltip参数
+  - `interactive`：可以点击tooltip内部的链接、表单等
+  - `trigger`：如何触发弹出，这里需要`triggerClose`设为`click`
+
 ```
 npm install
 npm run demo
--- entry.ts
+++ entry.ts
@@ -1,18 +1,19 @@
-import 'jquery';
 import $ from 'jquery';
 
 import 'tooltipster/dist/js/tooltipster.bundle';
 import 'tooltipster/dist/css/tooltipster.bundle.min.css';
 
 $('a').tooltipster({
-  side: 'bottom',
   content: 'loading ...',
   contentAsHTML: true,
-  delay: 200,
   interactive: true,
+  delay: 1000,
   trigger: 'custom',
   triggerOpen: {
     mouseenter: true
+  },
+  triggerClose: {
+    click: true
   },
   functionInit: (instance) => {
     console.log('> functionInit');
@@ -26,20 +27,22 @@ $('a').tooltipster({
   }
 })
 
-function clickedInsideTooltip(event: JQuery.Event): boolean {
-  const base = $(event.target).parents('.tooltipster-base')
-  return base.length > 0;
-}
-
-$(document).on('click', (event) => {
-  console.log('click on document:', event.target);
-
-  if (clickedInsideTooltip(event)) {
-    console.log('click inside tooltip, skip')
-  } else {
-    console.log('click outside tooltip, close all tooltips')
-    $.tooltipster.instances().forEach(it => {
-      it.close()
-    });
-  }
-});
+// Notice
+// Not needed, just set `interactive:true`
+// function clickedInsideTooltip(event: JQuery.Event): boolean {
+//   const base = $(event.target).parents('.tooltipster-base')
+//   return base.length > 0;
+// }
+//
+// $(document).on('click', (event) => {
+//   console.log('click on document:', event.target);
+//
+//   if (clickedInsideTooltip(event)) {
+//     console.log('click inside tooltip, skip')
+//   } else {
+//     console.log('click outside tooltip, close all tooltips')
+//     $.tooltipster.instances().forEach(it => {
+//       it.close()
+//     });
+//   }
+// });
